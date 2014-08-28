e7b8c8b2bc8930e1cc11270632730a8ca55fc054
smartrookie <zdf.smartrookie@gmail.com>
2014-08-28 13:41:27 +0800

    import Project

---
+++ ServerTest/SMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SMAppDelegate.h
+//  ServerTest
+//
+//  Created by jhbjserver on 14-8-28.
+//  Copyright (c) 2014年 smartrookie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ServerTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ServerTest
+//
+//  Created by jhbjserver on 14-8-28.
+//  Copyright (c) 2014年 smartrookie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
